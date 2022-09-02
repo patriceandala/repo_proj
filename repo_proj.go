8127c43682cd7c3da2ed12acd562c7095611f17f
Patrice Andala <patriceandala@gmail.com>
2022-09-02 14:49:02 +0300

    2

--- repo_proj.go
+++ repo_proj.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
-
-	log.Printf("Listening on port %s", port)
-	log.Printf("Open http://localhost:%s in the browser", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	fmt.Println("2222222222222222222222")
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
