3fa0f548964d8e4a8b945512de6c3ad66b1c3e3a
Patrice Andala <patriceandala@gmail.com>
2022-09-02 14:54:34 +0300

    222

--- repo_proj.go
+++ repo_proj.go
@@ -2,20 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
 func main() {
-	fmt.Println("2222222222222222222222")
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	_, err := fmt.Fprint(w, "Hello, World!")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+	fmt.Println("2222222222222222222222222222222222222222222")
 }
