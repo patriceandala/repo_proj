dfbbd38b1566642224e1935d68c66baf26f00153
Dfrent <patricedfrent@gmail.com>
2022-11-01 20:08:21 +0300

    Awesome

--- repo_proj.go
+++ repo_proj.go
@@ -1,10 +1,20 @@
 package main
 
+import "fmt"
+
 type Person struct {
 	name string
 	Age  string
 }
 
-func main() {
+func (p Person) printName() {
+	fmt.Println(p.name)
+}
 
+func main() {
+	p := Person{
+		name: "Patrice",
+		Age:  "32",
+	}
+	p.printName()
 }
