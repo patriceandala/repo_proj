a82e47fb167bc6a46d20ed7364241068eaedcc53
Dfrent <patricedfrent@gmail.com>
2022-11-04 12:31:06 +0300

    SQUASH

--- repo_proj.go
+++ repo_proj.go
@@ -11,19 +11,10 @@ type Person struct {
 }
 
 func main() {
-	fmt.Println(Person{
-		"Test", "Reb",
-	})
-	fmt.Println(Person{
-		"Squanch", "IrIsh",
-	})
-	fmt.Println(Person{
-		"Bird", "Person",
-	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		_, err := fmt.Fprintf(w, "What's good")
+		_, err := fmt.Fprintf(w, "What's good, n word -Stinkmener. Now reap what you sow nigga")
 		if err != nil {
 			return
 		}
